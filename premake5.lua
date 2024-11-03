cc886c2bf6907b9b79a054cc6c80096e624b5c31
Frank Kevin Zey <frank.kevin.zey@outlook.com>
2024-11-03 15:29:09 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "ImGui"
+	kind "StaticLib"
+	systemversion "latest"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "On"
+
+	targetdir (BIN .. "/%{prj.name}")
+	objdir (OBJ .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imgui_tables.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+
+	filter "system:linux"
+		pic "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
